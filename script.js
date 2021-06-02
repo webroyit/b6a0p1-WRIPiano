e0323626268eac46c36ea4f4e6bce1b643f90383
royweb <xytank@gmail.com>
2021-06-02 15:10:53 -0400

    Add logic to play the paino with keyboard

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
 const keys = document.querySelectorAll('.key');
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
 
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key));
 });
+
+// Play the piano on the keyboard
+document.addEventListener('keydown', e => {
+    const key = e.key;
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+    if (whiteKeyIndex > -1) {
+        playNote(whiteKeys[whiteKeyIndex]);
+    }
+    if (blackKeyIndex > -1) {
+        playNote(blackKeys[blackKeyIndex]);
+    }
+})
 
 // Play the audio
 function playNote(key) {
