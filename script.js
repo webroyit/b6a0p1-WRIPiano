3e2b550d696834e240cbbef95feacf79a93f159f
royweb <xytank@gmail.com>
2021-06-02 14:08:22 -0400

    Add logic to play the piano

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Test");
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => {
+    key.addEventListener('click', () => playNote(key));
+});
+
+// Play the audio
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note);
+    noteAudio.currentTime = 0;      // Make the audio start from the beginning
+    noteAudio.play();
+}
